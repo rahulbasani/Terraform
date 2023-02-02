cee0c9642069fa87e4f4e0c10993b681e2533569
unknown <rahulbasani98@gmail.com>
2023-02-02 10:33:10 +0000

    added backend S3

---
+++ root/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "statefilesterraform"
+    key    = "modules/terraform.tfstate"
+    region = "ap-south-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+}
